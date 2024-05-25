0a312c308e46ecb3d742cc99358cd067f4296a48
Amr Gad <merodiro@gmail.com>
2024-05-25 16:45:28 +0300

    use standalone mode for faster build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
