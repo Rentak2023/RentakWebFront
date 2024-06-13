9d5b48a6c0bd18968b45586a9bf93407008d97dc
Amr Gad <merodiro@gmail.com>
2024-06-13 15:28:02 +0300

    add form card

--- src/app/[locale]/(services)/step-form.tsx
+++ src/app/[locale]/(services)/step-form.tsx
@@ -1,11 +1,18 @@
 import { valibotResolver } from "@hookform/resolvers/valibot";
 import { format } from "date-fns";
-import { CalendarIcon, Loader2 } from "lucide-react";
+import { ArrowLeft, ArrowRight, CalendarIcon, Loader2 } from "lucide-react";
 import { useMemo } from "react";
 import { useForm, useFormContext, useWatch } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
   Form,
@@ -102,118 +109,130 @@ export function StepForm({ useFormStore, step, onSubmit }: StepFormProps) {
   };
 
   return (
-    <Form {...form}>
-      <form className="space-y-6" onSubmit={handleSubmit}>
-        {step.heading ? (
-          <h3 className="text-2xl font-semibold">
-            Acknowledgment And Commitment
-          </h3>
-        ) : null}
-        {step.list ? (
-          <ul className="list-disc">
-            {step.list.map((item) => (
-              <li key={item}>{item}</li>
-            ))}
-          </ul>
-        ) : null}
-        {filteredFields.map((formField) => (
-          <FormField
-            key={formField.name}
-            control={form.control}
-            name={formField.name}
-            render={({ field }) => (
-              <FormItem>
-                {formField.kind !== "checkbox" && (
-                  <FormLabel>{formField.label}</FormLabel>
-                )}
-                {formField.kind === "select" ? (
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <SelectTrigger>
-                        <SelectValue placeholder={formField.placeholder} />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {formField.options.map((option) => (
-                          <SelectItem value={option.value} key={option.value}>
-                            {option.label}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                ) : formField.kind === "checkbox" ? (
-                  <FormField
-                    control={form.control}
-                    name={formField.name}
-                    render={({ field }) => (
-                      <div>
-                        <FormControl className="me-2">
-                          <Checkbox
-                            checked={field.value}
-                            onCheckedChange={field.onChange}
-                          />
-                        </FormControl>
-                        <FormLabel>{formField.label}</FormLabel>
-                        {formField.description ? (
-                          <FormDescription>
-                            {formField.description}
-                          </FormDescription>
-                        ) : null}
-                      </div>
+    <Card>
+      {step.heading ? (
+        <CardHeader>
+          {step.heading ? (
+            <CardTitle className="text-2xl font-semibold">
+              Acknowledgment And Commitment
+            </CardTitle>
+          ) : null}
+          {step.list ? (
+            <ul className="mt-6 list-disc">
+              {step.list.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          ) : null}
+        </CardHeader>
+      ) : null}
+      <Form {...form}>
+        <form onSubmit={handleSubmit}>
+          <CardContent className={cn("space-y-6", !step.heading && "pt-6")}>
+            {filteredFields.map((formField) => (
+              <FormField
+                key={formField.name}
+                control={form.control}
+                name={formField.name}
+                render={({ field }) => (
+                  <FormItem>
+                    {formField.kind !== "checkbox" && (
+                      <FormLabel>{formField.label}</FormLabel>
                     )}
-                  />
-                ) : formField.kind === "date" ? (
-                  <Popover>
-                    <PopoverTrigger asChild>
+                    {formField.kind === "select" ? (
                       <FormControl>
-                        <Button
-                          variant="outline"
-                          className={cn(
-                            "flex w-full ps-3 text-start font-normal",
-                            !field.value && "text-muted-foreground",
-                          )}
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
                         >
-                          {field.value ? (
-                            format(field.value, "PPP")
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
-                          <CalendarIcon className="ms-auto size-4 opacity-50" />
-                        </Button>
+                          <SelectTrigger>
+                            <SelectValue placeholder={formField.placeholder} />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {formField.options.map((option) => (
+                              <SelectItem
+                                value={option.value}
+                                key={option.value}
+                              >
+                                {option.label}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
                       </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={field.value}
-                        onSelect={field.onChange}
-                        // disabled={(date) =>
-                        //   date > new Date() || date < new Date("1900-01-01")
-                        // }
-                        initialFocus
+                    ) : formField.kind === "checkbox" ? (
+                      <FormField
+                        control={form.control}
+                        name={formField.name}
+                        render={({ field }) => (
+                          <div>
+                            <FormControl className="me-2">
+                              <Checkbox
+                                checked={field.value}
+                                onCheckedChange={field.onChange}
+                              />
+                            </FormControl>
+                            <FormLabel>{formField.label}</FormLabel>
+                            {formField.description ? (
+                              <FormDescription>
+                                {formField.description}
+                              </FormDescription>
+                            ) : null}
+                          </div>
+                        )}
                       />
-                    </PopoverContent>
-                  </Popover>
-                ) : (
-                  <FormControl>
-                    <Input type={formField.type} {...field} />
-                  </FormControl>
+                    ) : formField.kind === "date" ? (
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant="outline"
+                              className={cn(
+                                "flex w-full ps-3 text-start font-normal",
+                                !field.value && "text-muted-foreground",
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ms-auto size-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            // disabled={(date) =>
+                            //   date > new Date() || date < new Date("1900-01-01")
+                            // }
+                            initialFocus
+                          />
+                        </PopoverContent>
+                      </Popover>
+                    ) : (
+                      <FormControl>
+                        <Input type={formField.type} {...field} />
+                      </FormControl>
+                    )}
+                    {formField.description ? (
+                      <FormDescription>{formField.description}</FormDescription>
+                    ) : null}
+                    <FormMessage />
+                  </FormItem>
                 )}
-                {formField.description ? (
-                  <FormDescription>{formField.description}</FormDescription>
-                ) : null}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        ))}
-
-        <StepperFormActions onPrevStep={handlePrevStep} />
-      </form>
-    </Form>
+              />
+            ))}
+          </CardContent>
+          <CardFooter>
+            <StepperFormActions onPrevStep={handlePrevStep} />
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
   );
 }
 
@@ -237,7 +256,7 @@ function StepperFormActions({ onPrevStep }: StepperFormActions) {
     : "Submit";
 
   return (
-    <div className="flex w-full justify-end gap-2">
+    <div className="flex w-full justify-between">
       {hasCompletedAllSteps ? (
         <Button size="sm" onClick={resetSteps}>
           Reset
@@ -248,9 +267,10 @@ function StepperFormActions({ onPrevStep }: StepperFormActions) {
             disabled={isDisabledStep}
             onClick={onPrevStep}
             size="sm"
-            variant="ghost"
+            variant="outline"
             type="button"
           >
+            <ArrowLeft className="me-2 size-4" />
             Prev
           </Button>
           <Button
@@ -261,7 +281,11 @@ function StepperFormActions({ onPrevStep }: StepperFormActions) {
             {form.formState.isSubmitting ? (
               <Loader2 className="mr-2 size-4 animate-spin" />
             ) : null}
+
             {isLastStep ? submitMessage : isOptionalStep ? "Skip" : "Next"}
+            {!isLastStep && !isOptionalStep && (
+              <ArrowRight className="ms-2 size-4" />
+            )}
           </Button>
         </>
       )}
