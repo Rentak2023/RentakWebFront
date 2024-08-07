e95f006ea4d49a83c17518ef61c0ad12fe726c5a
Amr Gad <merodiro@gmail.com>
2024-08-07 15:29:49 +0300

    add valu and mastercard logos

--- src/components/home/partners.tsx
+++ src/components/home/partners.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 import forsaLogo from "@/app/[locale]/assets/images/logos/forsa.jpg";
 import halanLogo from "@/app/[locale]/assets/images/logos/halan.png";
+import mastercardLogo from "@/app/[locale]/assets/images/logos/mastercard.png";
 import orionLogo from "@/app/[locale]/assets/images/logos/orion.png";
 import palmHillsLogo from "@/app/[locale]/assets/images/logos/palm-hills.png";
 import paytabsLogo from "@/app/[locale]/assets/images/logos/paytabs.jpg";
 import souhoolaLogo from "@/app/[locale]/assets/images/logos/souhoola.png";
+import valuLogo from "@/app/[locale]/assets/images/logos/valu.png";
 
 const partners = [
   {
@@ -32,6 +34,14 @@ const partners = [
     name: "palm hills",
     logo: palmHillsLogo,
   },
+  {
+    name: "mastercard",
+    logo: mastercardLogo,
+  },
+  {
+    name: "valu",
+    logo: valuLogo,
+  },
 ];
 
 export function Partners() {
@@ -40,7 +50,7 @@ export function Partners() {
       <h3 className="text-center text-xl font-medium text-slate-700">
         Trusted by
       </h3>
-      <div className="mt-8 grid items-center justify-items-center gap-8 lg:grid-cols-6">
+      <div className="mt-8 grid items-center justify-items-center gap-12 lg:grid-cols-8">
         {partners.map((partner) => (
           <Image
             key={partner.name}
