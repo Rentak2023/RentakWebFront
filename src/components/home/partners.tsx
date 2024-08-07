73295c33e40cd324afb9e9052fe1817c451081b7
Amr Gad <merodiro@gmail.com>
2024-08-07 15:36:31 +0300

    add visa logo

--- src/components/home/partners.tsx
+++ src/components/home/partners.tsx
@@ -8,6 +8,7 @@ import palmHillsLogo from "@/app/[locale]/assets/images/logos/palm-hills.png";
 import paytabsLogo from "@/app/[locale]/assets/images/logos/paytabs.jpg";
 import souhoolaLogo from "@/app/[locale]/assets/images/logos/souhoola.png";
 import valuLogo from "@/app/[locale]/assets/images/logos/valu.png";
+import visaLogo from "@/app/[locale]/assets/images/logos/visa.png";
 
 const partners = [
   {
@@ -42,6 +43,10 @@ const partners = [
     name: "valu",
     logo: valuLogo,
   },
+  {
+    name: "visa",
+    logo: visaLogo,
+  },
 ];
 
 export function Partners() {
@@ -50,7 +55,7 @@ export function Partners() {
       <h3 className="text-center text-xl font-medium text-slate-700">
         Trusted by
       </h3>
-      <div className="mt-8 grid items-center justify-items-center gap-12 lg:grid-cols-8">
+      <div className="mt-8 grid items-center justify-items-center gap-12 lg:grid-cols-9">
         {partners.map((partner) => (
           <Image
             key={partner.name}
