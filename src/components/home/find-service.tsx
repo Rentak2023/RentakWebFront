8b82c6215614eecb4548e3953bc2c58d92ef9b0b
Amr Gad <merodiro@gmail.com>
2024-08-18 18:26:57 +0300

    localize services

--- src/components/home/find-service.tsx
+++ src/components/home/find-service.tsx
@@ -1,82 +1,77 @@
 "use client";
 
 import { SquareCheckBigIcon } from "lucide-react";
+import { useLocale, useTranslations } from "next-intl";
 import { useState } from "react";
 
 type Question = {
   title: string;
-  answers?: Array<{ label: string; question?: Question }>;
+  arTitle: string;
+  answers?: Array<{ label: string; arLabel: string; question?: Question }>;
   services?: Array<Service>;
 };
 
 type Service = {
   title: string;
-  description: string;
+  arTitle: string;
   // href: string;
 };
 
 const questions: Question = {
   title: "Are you a property Owner, a Tenant or a Broker?",
+  arTitle: "هل أنت مالك، مستأجر، أو بروكر؟",
   answers: [
     {
       label: "Owner",
+      arLabel: "مالك",
       question: {
         title: "Are you already renting your property or want to rent yours?",
+        arTitle: "هل تقوم بتأجير عقارك حاليا أم ترغب في تأجيره؟",
         answers: [
           {
             label: "Renting Already",
+            arLabel: "تقوم بالتأجير حاليا",
             question: {
               title: "Services",
+              arTitle: "الخدمات",
               services: [
                 {
                   title: "Rent Collection",
-                  description:
-                    "Rentak can collect your rent efficiently and ensure timely payments.",
+                  arTitle: "تحصيل الإيجار",
                 },
                 {
-                  title: "Finance Maintenance",
-                  description:
-                    "Manage or finance your property’s annual maintenance fees to the developer.",
+                  title: "Pay/Finance Annual Maintenance",
+                  arTitle: "دفع تقسيط الصيانة السنوية",
                 },
                 {
-                  title: "Evaluate Property",
-                  description:
-                    "Get a professional evaluation of your property’s  rental value.",
+                  title: "Evaluate My Property",
+                  arTitle: "تقييم الوحدة",
                 },
               ],
             },
           },
           {
             label: "Want to Rent My Property",
+            arLabel: "رغب في تأجير عقارك",
             question: {
               title: "Services",
+              arTitle: "الخدمات",
               services: [
                 {
-                  title: "Rent Contract",
-                  description:
-                    "Create a legally compliant rental contract for your property.",
+                  title: "Construct Free Contract",
+                  arTitle: "إنشاء عقد ايجار",
                 },
                 {
-                  title: "Finance Maintenance",
-                  description:
-                    "Pay or finance your property’s annual maintenance fees.",
+                  title: "Pay/Finance Annual Maintenance",
+                  arTitle: "دفع/تمويل الصيانة السنوية",
                 },
                 {
-                  title: "Evaluate Property",
-                  description:
-                    "Get a professional evaluation of your property’s rental value.",
+                  title: "Evaluate My Property",
+                  arTitle: "تقييم عقاري",
                 },
                 {
-                  title: "Rent Unit",
-                  description: `Choose from the following options to rent your property:
-I. Brokerage: Use Rentak's brokerage services to find suitable
-tenants.
-II. Basic Rental Management: Rentak will manage the rental
-process, including tenant screening and rent collection.
-III. Guaranteed Rental Management: Ensure stable rental
-income with Rentak's guaranteed rent management service.
-IV. Instant Rent: Rentak will rent your property instantly from
-you.`,
+                  title: "Rent My Unit",
+                  arTitle: "تأجير وحدتي",
                 },
               ],
             },
@@ -86,54 +81,53 @@ you.`,
     },
     {
       label: "Tenant",
+      arLabel: "مستأجر",
       question: {
         title:
           "Are you looking for a property to rent or need help with your current rental?",
+        arTitle:
+          "هل تبحث عن عقار للإيجار أم تحتاج إلى مساعدة في إيجارك الحالي؟",
         answers: [
           {
             label: "Looking for a Property",
+            arLabel: "تبحث عن عقار",
             question: {
               title: "Services",
+              arTitle: "الخدمات",
               services: [
                 {
                   title: "Listing Platform",
-                  description:
-                    "Browse a wide range of verified rental properties.",
+                  arTitle: "منصة القوائم",
                 },
                 {
                   title: "Request Property",
-                  description:
-                    "Submit your preferences and let Rentak match you  with suitable listings.",
+                  arTitle: "طلب عقار",
                 },
               ],
             },
           },
           {
             label: "Need Help with Current Rental",
+            arLabel: "تحتاج إلى مساعدة في إيجارك الحالي",
             question: {
               title: "Services",
+              arTitle: "الخدمات",
               services: [
                 {
                   title: "Pay Rent",
-                  description:
-                    "Conveniently pay your rent through multiple payment methods.",
+                  arTitle: "دفع الإيجار",
                 },
                 {
                   title: "Pay Maintenance",
-                  description:
-                    "Handle maintenance payments directly through Rentak.",
+                  arTitle: "دفع الصيانة",
                 },
                 // {
                 //   // TODO: disable this option
                 //   title: "Bill Payments",
-                //   description:
-                //     "Manage and pay utility and service bills seamlessly.",
                 // },
                 // // TODO: disable this option
                 // {
                 //   title: "Order Facility Management Service",
-                //   description:
-                //     "Request professional maintenance services for your property.",
                 // },
               ],
             },
@@ -143,47 +137,48 @@ you.`,
     },
     {
       label: "Broker",
+      arLabel: "بروكر",
       question: {
         title:
-          "Are you looking for Exclusive Listings or faciliate you rental deal?",
+          "Are you looking for Exclusive Listings or facilitate you rental deal?",
+        arTitle: "هل تبحث عن قوائم حصرية أم تسهيل صفقة الإيجار؟",
         answers: [
           {
             label: "Looking for Exclusive Listings",
+            arLabel: "تبحث عن قوائم حصرية",
             question: {
               title: "Services",
+              arTitle: "الخدمات",
               services: [
                 {
                   title: "Access Exclusive Listings",
-                  description:
-                    "Gain access to exclusive property listings available only through Rentak.",
+                  arTitle: "الوصول إلى القوائم الحصرية",
                 },
                 {
                   title: "Promote Properties",
-                  description:
-                    "Use Rentak’s marketing tools to promote properties effectively.",
+                  arTitle: "ترويج العقارات",
                 },
                 {
                   title: "Request Property",
-                  description:
-                    "Submit your preferences and let Rentak match you with suitable listings.",
+                  arTitle: "طلب عقار",
                 },
               ],
             },
           },
           {
             label: "Facilitate you rental deal",
+            arLabel: "تسهيل صفقة الإيجار",
             question: {
               title: "Services",
+              arTitle: "الخدمات",
               services: [
                 {
-                  title: "Rent Contract",
-                  description:
-                    "Quickly and easily create legally compliant rental contracts.",
+                  title: "Construct Free Contract",
+                  arTitle: "إنشاء عقد ايجار",
                 },
                 {
                   title: "Finance Brokerage Commission",
-                  description:
-                    "Ensure you earn your full brokerage commissions by financing the amount to landlords and tenants, with Rentak transferring the amount directly to your bank.",
+                  arTitle: "تمويل عمولة الوساطة",
                 },
               ],
             },
@@ -196,14 +191,18 @@ you.`,
 
 export function FindService() {
   const [currentQuestion, setCurrentQuestion] = useState(questions);
+  const t = useTranslations("find-service");
+  const locale = useLocale();
 
   return (
     <div className="mb-12 flex-1 rounded-3xl bg-white/85 p-6 text-center lg:mb-0">
       <h3 className="text-xl font-medium text-primary-800">
-        Find the perfect service with <span className="font-bold">rentak</span>
+        {t.rich("title", {
+          bold: (text) => <span className="font-bold">{text}</span>,
+        })}
       </h3>
       <h2 className="mt-4 text-3xl font-semibold text-primary-900">
-        {currentQuestion.title}
+        {locale === "en" ? currentQuestion.title : currentQuestion.arTitle}
       </h2>
       <div className="mt-12 flex flex-col gap-3">
         {currentQuestion.answers?.map((answer, index) => (
@@ -219,7 +218,7 @@ export function FindService() {
             <span className="flex size-8 items-center justify-center rounded-full border border-slate-700 bg-white p-2 leading-8 text-slate-700">
               {String.fromCodePoint(65 + index)}
             </span>
-            <span>{answer.label}</span>
+            <span>{locale === "en" ? answer.label : answer.arLabel}</span>
           </button>
         ))}
         {currentQuestion.services?.map((answer) => (
@@ -228,7 +227,7 @@ export function FindService() {
             key={answer.title}
           >
             <SquareCheckBigIcon className="size-6" />
-            {answer.title}
+            {locale === "en" ? answer.title : answer.arTitle}
           </button>
         ))}
       </div>
