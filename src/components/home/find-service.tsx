83a7a0bf782d7bde5bb331b8a520293df6905bb8
Amr Gad <merodiro@gmail.com>
2024-08-20 14:15:53 +0300

    update servies titles again

--- src/components/home/find-service.tsx
+++ src/components/home/find-service.tsx
@@ -9,8 +9,14 @@ import { Link } from "@/navigation";
 type Question = {
   title: string;
   arTitle: string;
-  answers?: Array<{ label: string; arLabel: string; question?: Question }>;
+  answers?: Array<Answer>;
   services?: Array<Service>;
+};
+
+type Answer = {
+  title: string;
+  arTitle: string;
+  question?: Question;
 };
 
 type Service = {
@@ -19,23 +25,25 @@ type Service = {
   href: string;
 };
 
+const isService = (item: Answer | Service): item is Service => "href" in item;
+
 const questions: Question = {
   title: "Are you a property Owner, Tenant or Broker?",
   arTitle: "هل أنت مالك، مستأجر، أو بروكر؟",
   answers: [
     {
-      label: "Owner",
-      arLabel: "مالك",
+      title: "Owner",
+      arTitle: "مالك",
       question: {
         title: "Are you already renting your property or want to rent yours?",
         arTitle: "هل تقوم بتأجير عقارك حاليا أم ترغب في تأجيره؟",
         answers: [
           {
-            label: "Renting Already",
-            arLabel: "تقوم بالتأجير حاليا",
+            title: "Renting Already",
+            arTitle: "تقوم بالتأجير حاليا",
             question: {
-              title: "Services",
-              arTitle: "الخدمات",
+              title: "Your Best-Match Rental Solutions",
+              arTitle: "أفضل حلول إيجار تناسب احتياجاتك",
               services: [
                 {
                   title: "Rent Collection",
@@ -56,12 +64,17 @@ const questions: Question = {
             },
           },
           {
-            label: "Want to Rent My Property",
-            arLabel: "رغب في تأجير عقارك",
+            title: "Want to Rent My Property",
+            arTitle: "رغب في تأجير عقارك",
             question: {
-              title: "Services",
-              arTitle: "الخدمات",
+              title: "Your Best-Match Rental Solutions",
+              arTitle: "أفضل حلول إيجار تناسب احتياجاتك",
               services: [
+                {
+                  title: "Rent My Unit",
+                  arTitle: "تأجير وحدتي",
+                  href: "/",
+                },
                 {
                   title: "Create Rent Contract",
                   arTitle: "إنشاء عقد ايجار",
@@ -77,11 +90,6 @@ const questions: Question = {
                   arTitle: "تقييم عقاري",
                   href: "/",
                 },
-                {
-                  title: "Rent My Unit",
-                  arTitle: "تأجير وحدتي",
-                  href: "/",
-                },
               ],
             },
           },
@@ -89,8 +97,8 @@ const questions: Question = {
       },
     },
     {
-      label: "Tenant",
-      arLabel: "مستأجر",
+      title: "Tenant",
+      arTitle: "مستأجر",
       question: {
         title:
           "Are you looking for a property to rent or need help with your current rental?",
@@ -98,11 +106,11 @@ const questions: Question = {
           "هل تبحث عن عقار للإيجار أم تحتاج إلى مساعدة في إيجارك الحالي؟",
         answers: [
           {
-            label: "Looking for a Property",
-            arLabel: "تبحث عن عقار",
+            title: "Looking for a Property",
+            arTitle: "تبحث عن عقار",
             question: {
-              title: "Services",
-              arTitle: "الخدمات",
+              title: "Your Best-Match Rental Solutions",
+              arTitle: "أفضل حلول إيجار تناسب احتياجاتك",
               services: [
                 {
                   title: "Listing Platform",
@@ -117,51 +125,58 @@ const questions: Question = {
               ],
             },
           },
+          // {
+          //   label: "Pay My Rent",
+          //   arLabel: "تحتاج إلى مساعدة في إيجارك الحالي",
+          //   question: {
+          //     title: "Your Best-Match Rental Solutions",
+          //     arTitle: "أفضل حلول إيجار تناسب احتياجاتك",
+          //     services: [
+          //       {
+          //         title: "Pay Rent",
+          //         arTitle: "دفع الإيجار",
+          //         href: "/rent-payment",
+          //       },
+          //       {
+          //         title: "Pay Maintenance",
+          //         arTitle: "دفع الصيانة",
+          //         href: "/maintenance-payment",
+          //       },
+          //       // {
+          //       //   // TODO: disable this option
+          //       //   title: "Bill Payments",
+          //       // },
+          //       // // TODO: disable this option
+          //       // {
+          //       //   title: "Order Facility Management Service",
+          //       // },
+          //     ],
+          //   },
+          // },
+        ],
+        services: [
           {
-            label: "Pay My Rent",
-            arLabel: "تحتاج إلى مساعدة في إيجارك الحالي",
-            question: {
-              title: "Services",
-              arTitle: "الخدمات",
-              services: [
-                {
-                  title: "Pay Rent",
-                  arTitle: "دفع الإيجار",
-                  href: "/rent-payment",
-                },
-                {
-                  title: "Pay Maintenance",
-                  arTitle: "دفع الصيانة",
-                  href: "/maintenance-payment",
-                },
-                // {
-                //   // TODO: disable this option
-                //   title: "Bill Payments",
-                // },
-                // // TODO: disable this option
-                // {
-                //   title: "Order Facility Management Service",
-                // },
-              ],
-            },
+            title: "Pay Rent",
+            arTitle: "دفع الإيجار",
+            href: "/rent-payment",
           },
         ],
       },
     },
     {
-      label: "Broker",
-      arLabel: "بروكر",
+      title: "Broker",
+      arTitle: "بروكر",
       question: {
         title:
           "Are you looking for Exclusive Listings or facilitate your rental deal?",
         arTitle: "هل تبحث عن قوائم حصرية أم تسهيل صفقة الإيجار؟",
         answers: [
           {
-            label: "Looking for Exclusive Listings",
-            arLabel: "تبحث عن قوائم حصرية",
+            title: "Looking for Exclusive Listings",
+            arTitle: "تبحث عن قوائم حصرية",
             question: {
-              title: "Services",
-              arTitle: "الخدمات",
+              title: "Your Best-Match Rental Solutions",
+              arTitle: "أفضل حلول إيجار تناسب احتياجاتك",
               services: [
                 {
                   title: "Access Exclusive Listings",
@@ -182,11 +197,11 @@ const questions: Question = {
             },
           },
           {
-            label: "Facilitate your rental deal",
-            arLabel: "تسهيل صفقة الإيجار",
+            title: "Facilitate your rental deal",
+            arTitle: "تسهيل صفقة الإيجار",
             question: {
-              title: "Services",
-              arTitle: "الخدمات",
+              title: "Your Best-Match Rental Solutions",
+              arTitle: "أفضل حلول إيجار تناسب احتياجاتك",
               services: [
                 {
                   title: "Construct Rent Contract",
@@ -212,6 +227,11 @@ export function FindService() {
   const t = useTranslations("find-service");
   const locale = useLocale();
 
+  const options = [
+    ...(currentQuestion.answers ?? []),
+    ...(currentQuestion.services ?? []),
+  ];
+
   return (
     <div className="mb-12 flex-1 rounded-3xl bg-white/70 p-6 text-center lg:mb-0">
       <h3 className="text-xl font-medium capitalize text-slate-800">
@@ -223,34 +243,39 @@ export function FindService() {
         {locale === "en" ? currentQuestion.title : currentQuestion.arTitle}
       </h2>
       <div className="mt-12 flex flex-col gap-3">
-        {currentQuestion.answers?.map((answer, index) => (
-          <button
-            onClick={() => {
-              if (answer.question) {
-                setCurrentQuestion(answer.question);
-              }
-            }}
-            className="flex flex-row items-center justify-start gap-2 rounded-3xl bg-white p-4 text-slate-700 transition duration-300 hover:bg-primary-800/80 hover:text-white"
-            key={answer.label}
-          >
-            <span className="flex size-8 items-center justify-center rounded-full border border-slate-700 bg-white p-2 leading-8 text-slate-700">
-              {String.fromCodePoint(65 + index)}
-            </span>
-            <span className="capitalize">
-              {locale === "en" ? answer.label : answer.arLabel}
-            </span>
-          </button>
-        ))}
-        {currentQuestion.services?.map((answer) => (
-          <Link
-            key={answer.title}
-            className="flex items-center justify-start gap-2 rounded-3xl bg-white p-4 capitalize text-slate-700 transition duration-300 hover:bg-primary-800/80 hover:text-white"
-            href={answer.href}
-          >
-            <SquareCheckBigIcon className="size-6" />
-            {locale === "en" ? answer.title : answer.arTitle}
-          </Link>
-        ))}
+        {options.map((option, index) =>
+          isService(option) ? (
+            <Link
+              key={option.title}
+              className="flex flex-row items-center justify-start gap-2 rounded-3xl bg-white p-4 text-slate-700 transition duration-300 hover:bg-primary-800/80 hover:text-white"
+              href={option.href}
+            >
+              <span className="flex size-8 items-center justify-center rounded-full border border-slate-700 bg-white p-2 leading-8 text-slate-700">
+                {String.fromCodePoint(65 + index)}
+              </span>
+              <span className="capitalize">
+                {locale === "en" ? option.title : option.arTitle}
+              </span>
+            </Link>
+          ) : (
+            <button
+              onClick={() => {
+                if (option.question) {
+                  setCurrentQuestion(option.question);
+                }
+              }}
+              className="flex flex-row items-center justify-start gap-2 rounded-3xl bg-white p-4 text-slate-700 transition duration-300 hover:bg-primary-800/80 hover:text-white"
+              key={option.title}
+            >
+              <span className="flex size-8 items-center justify-center rounded-full border border-slate-700 bg-white p-2 leading-8 text-slate-700">
+                {String.fromCodePoint(65 + index)}
+              </span>
+              <span className="capitalize">
+                {locale === "en" ? option.title : option.arTitle}
+              </span>
+            </button>
+          ),
+        )}
       </div>
     </div>
   );
