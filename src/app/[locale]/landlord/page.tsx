114c9fe70937bbff048944672ecacba2f6762ddf
Amr Gad <merodiro@gmail.com>
2024-09-10 14:59:06 +0300

    add why rentak section

--- src/app/[locale]/landlord/page.tsx
+++ src/app/[locale]/landlord/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import WhyRentak from "./components/why-rentak";
 
 const Landlord = () => {
   return (
     <main className="min-h-screen">
       <Header />
+      <WhyRentak />
     </main>
   );
 };
