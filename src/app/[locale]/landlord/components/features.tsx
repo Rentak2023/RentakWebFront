bd39646f15bdaa2c5dc344e60200138e8a277ff9
Amr Gad <merodiro@gmail.com>
2024-10-01 18:05:11 +0300

    update features section

--- src/app/[locale]/landlord/components/features.tsx
+++ src/app/[locale]/landlord/components/features.tsx
@@ -12,6 +12,7 @@ const features = [
       "Let us handle the day-to-day management of your property. From tenant screening to rent collection till the end of your rental contract, Rentak ensures your property is well-managed and tenants are happy.",
     cta: "Simplify Property Management",
     icon: shieldIcon,
+    href: "/",
   },
   {
     title: "Rent Collection",
@@ -19,6 +20,7 @@ const features = [
       "Automate your rent collection process with our secure and reliable system. Receive payments on time, every time, with full transparency and detailed reporting.",
     cta: "Start Collecting with Ease",
     icon: shieldIcon,
+    href: "/",
   },
   {
     title: "Promote Your Property",
@@ -26,6 +28,7 @@ const features = [
       "Reach more potential tenants by promoting your property on Rentak. Our platform connects you with high-quality brokers and tenants faster.",
     cta: "Promote My Property",
     icon: shieldIcon,
+    href: "/",
   },
   {
     title: "Property Valuation",
@@ -33,6 +36,7 @@ const features = [
       "Understand the true value of your property with our comprehensive valuation service. Get insights into market trends and rental potential to make informed decisions.",
     cta: "Get Your Property Valued",
     icon: shieldIcon,
+    href: "/",
   },
   {
     title: "Legal Contract Creation",
@@ -40,6 +44,7 @@ const features = [
       "Create rental contract in a minute. Our contracts are tailored to protect your interests and comply with all relevant regulations.",
     cta: "Create Your Contract",
     icon: shieldIcon,
+    href: "/",
   },
   {
     title: "Pay Annual Maintenance Fees",
@@ -47,6 +52,7 @@ const features = [
       "Easily manage and pay your property’s annual maintenance fees directly through Rentak. Through our various payment methods, you can pay your fees from 6-60 months, so you avoid any penalties.",
     cta: "Manage Maintenance Fees",
     icon: shieldIcon,
+    href: "/",
   },
   //
   // {
@@ -77,28 +83,67 @@ const features = [
 
 const Features = () => {
   return (
-    <div className="mt-32 bg-primary-100/40 py-28">
-      <div className="container mx-auto grid gap-8 px-4 md:grid-cols-2 lg:grid-cols-3 xl:max-w-7xl">
-        {features.map((feature) => (
-          <div
-            className="mx-auto flex max-w-7xl flex-col items-start "
-            key={feature.title}
-          >
-            <span className=" rounded-full bg-white p-2 shadow-lg">
-              <Image src={feature.icon} alt="" />
-            </span>
-            <h3 className="mt-6 text-xl font-semibold tracking-tight text-primary-900">
-              {feature.title}
-            </h3>
-            <p className="mb-6 mt-3 text-slate-700">{feature.description}</p>
-            <a
-              href="/"
-              className="mt-auto inline-flex items-center justify-center  rounded-full bg-primary-600 px-4 py-2 font-medium text-slate-50 ring-offset-white transition-colors hover:bg-primary-600/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600/90 focus-visible:ring-offset-2"
+    <div className="mt-32 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-capitalized text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+            Services that we support
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+            voluptatum cupiditate veritatis in accusamus quisquam.
+          </p>
+        </div>
+        <div className="mx-auto mt-8 max-w-2xl sm:mt-12 lg:mt-16 lg:max-w-none">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+            {features.map((feature) => (
+              <div key={feature.title} className="flex flex-col">
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <div className="mb-6 flex size-10 items-center justify-center rounded-lg bg-primary-700">
+                    {/* <feature.icon
+                      aria-hidden="true"
+                      className="size-6 text-white"
+                    /> */}
+                  </div>
+                  {feature.title}
+                </dt>
+                <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                  <p className="flex-auto">{feature.description}</p>
+                  <p className="mt-4">
+                    <a
+                      href={feature.href}
+                      className="text-sm font-semibold leading-6 text-primary-700"
+                    >
+                      {feature.cta} <span aria-hidden="true">→</span>
+                    </a>
+                  </p>
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+        {/* <div className="container mx-auto grid gap-8 px-4 md:grid-cols-2 lg:grid-cols-3 xl:max-w-7xl">
+          {features.map((feature) => (
+            <div
+              className="mx-auto flex max-w-7xl flex-col items-start "
+              key={feature.title}
             >
-              {feature.cta}
-            </a>
-          </div>
-        ))}
+              <span className=" rounded-full bg-white p-2 shadow-lg">
+                <Image src={feature.icon} alt="" />
+              </span>
+              <h3 className="mt-6 text-xl font-semibold tracking-tight text-primary-900">
+                {feature.title}
+              </h3>
+              <p className="mb-6 mt-3 text-slate-700">{feature.description}</p>
+              <a
+                href="/"
+                className="mt-auto inline-flex items-center justify-center  rounded-full bg-primary-600 px-4 py-2 font-medium text-slate-50 ring-offset-white transition-colors hover:bg-primary-600/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600/90 focus-visible:ring-offset-2"
+              >
+                {feature.cta}
+              </a>
+            </div>
+          ))}
+        </div> */}
       </div>
     </div>
   );
