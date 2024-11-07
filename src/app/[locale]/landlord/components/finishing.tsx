4caf221d90d90ff9948cd3404512c045204e42a7
Amr Gad <merodiro@gmail.com>
2024-11-07 13:25:50 +0200

    update finishing content

--- src/app/[locale]/landlord/components/finishing.tsx
+++ src/app/[locale]/landlord/components/finishing.tsx
@@ -1,4 +1,10 @@
+import Image from "next/image";
 import { useTranslations } from "next-intl";
+
+import img01 from "./images/01.jpg";
+import img02 from "./images/02.jpg";
+import img03 from "./images/03.jpg";
+import img04 from "./images/04.jpg";
 
 export default function Finishing() {
   const t = useTranslations("landlord.finishing");
@@ -17,32 +23,32 @@ export default function Finishing() {
           </div>
           <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
             <div className="w-0 flex-auto lg:ms-auto lg:w-auto lg:flex-none lg:self-end">
-              <img
+              <Image
                 alt=""
-                src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80"
+                src={img01}
                 className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-slate-50 object-cover"
               />
             </div>
             <div className="contents lg:col-span-2 lg:col-end-2 lg:ms-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
               <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
-                <img
+                <Image
                   alt=""
-                  src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                  src={img02}
                   className="aspect-[4/3] w-96 max-w-none flex-none rounded-2xl bg-slate-50 object-cover"
                 />
               </div>
               <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
-                <img
+                <Image
                   alt=""
-                  src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80"
+                  src={img03}
                   className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-slate-50 object-cover"
                 />
               </div>
               <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
-                <img
+                <Image
                   alt=""
-                  src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
-                  className="aspect-[4/3] w-96 max-w-none rounded-2xl bg-slate-50 object-cover"
+                  src={img04}
+                  className="aspect-square w-96 max-w-none rounded-2xl bg-slate-50 object-cover"
                 />
               </div>
             </div>
