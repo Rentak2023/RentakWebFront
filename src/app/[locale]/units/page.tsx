1faaf959f0e6d812a4025e4952a585e64e7de455
Amr Gad <merodiro@gmail.com>
2024-12-23 01:48:07 +0200

    fetch data on server

--- src/app/[locale]/units/page.tsx
+++ src/app/[locale]/units/page.tsx
@@ -1,19 +1,9 @@
 import { setRequestLocale } from "next-intl/server";
-import { createSearchParamsCache } from "nuqs/server";
 import { type SearchParams } from "nuqs/server";
 
 import Properties from "@/components/units/properties/properties";
 import SearchForm from "@/components/units/search-form/search-form";
-import { citiesQuery, districtsQuery } from "@/queries/location";
-import {
-  finishTypesQuery,
-  minMaxPriceQuery,
-  propertyTypesQuery,
-  unitsQuery,
-} from "@/queries/units";
 import { propertiesQueryCache } from "@/services/properties";
-
-import { makeQueryClient } from "../get-query-client";
 
 export default async function UnitsPage(
   props: Readonly<{
@@ -24,27 +14,11 @@ export default async function UnitsPage(
   const searchParams = await props.searchParams;
   const params = await props.params;
 
-  const parsedParams = propertiesQueryCache.parse(searchParams);
+  propertiesQueryCache.parse(searchParams);
 
   const { locale } = params;
 
   setRequestLocale(locale);
-  const queryClient = makeQueryClient();
-
-  queryClient.prefetchQuery(minMaxPriceQuery);
-  queryClient.prefetchQuery(citiesQuery);
-  queryClient.prefetchQuery(finishTypesQuery(locale));
-  queryClient.prefetchQuery(propertyTypesQuery(locale));
-  queryClient.prefetchQuery(
-    unitsQuery({
-      ...searchParams,
-      lang: locale,
-    }),
-  );
-
-  if (searchParams.governoment_id) {
-    queryClient.prefetchQuery(districtsQuery(parsedParams.governoment_id));
-  }
 
   return (
     <main className="min-h-screen">
