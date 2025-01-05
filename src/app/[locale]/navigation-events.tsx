d22fbcff9fbd7fc52ae79006604651d13c8ed96d
Amr Gad <merodiro@gmail.com>
2025-01-05 14:11:24 +0200

    fix navigation events

--- src/app/[locale]/navigation-events.tsx
+++ src/app/[locale]/navigation-events.tsx
@@ -5,43 +5,44 @@ import "nprogress/nprogress.css";
 // eslint-disable-next-line no-restricted-imports
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import nProgress from "nprogress";
-import { useEffect } from "react";
+import { useEffect, useTransition } from "react";
 
 export function NavigationEvents() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     const _push = router.push.bind(router);
     const _replace = router.replace.bind(router);
 
-    function progressStart(href: string) {
-      const url = `${pathname}${searchParams.size > 0 ? `?${searchParams.toString()}` : ""}`;
-      if (href !== url) {
-        nProgress.start();
-      }
-    }
-
     // eslint-disable-next-line react-compiler/react-compiler
     router.push = (href, options) => {
-      progressStart(href);
-      _push(href, options);
+      startTransition(() => {
+        nProgress.start();
+        _push(href, options);
+      });
     };
 
     router.replace = (href, options) => {
-      progressStart(href);
-      _replace(href, options);
+      startTransition(() => {
+        nProgress.start();
+        _replace(href, options);
+      });
     };
+
     return () => {
       router.push = _push;
       router.replace = _replace;
     };
-  }, [pathname, router, searchParams]);
+  }, [router]);
 
   useEffect(() => {
-    nProgress.done();
-  }, [pathname, searchParams]);
+    if (!isPending) {
+      nProgress.done();
+    }
+  }, [isPending, pathname, searchParams]);
 
   return null;
 }
