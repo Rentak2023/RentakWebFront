bedde0376bd48b47f2407215904946f6599e8e57
Amr Gad <merodiro@gmail.com>
2025-01-30 15:10:26 +0200

    update unit modal

--- src/app/[locale]/(dashboard)/dashboard/unit-modal.tsx
+++ src/app/[locale]/(dashboard)/dashboard/unit-modal.tsx
@@ -82,7 +82,9 @@ export function UnitModal({ unitId }: UnitModalProps) {
     <DialogContent className="max-h-dvh max-w-7xl overflow-y-auto">
       <div className="flex flex-col gap-y-4 lg:flex-row lg:justify-between">
         <div className="flex items-center gap-4">
-          {/* <div className="size-20 rounded-full bg-slate-400"></div> */}
+          <div className="rounded-full p-4 shadow-md">
+            <HomeIcon className="size-8 text-slate-800" />
+          </div>
           <div>
             <DialogTitle className="text-primary-900 text-xl font-semibold">
               {contract.unit.name}
@@ -103,7 +105,7 @@ export function UnitModal({ unitId }: UnitModalProps) {
           <div className="flex flex-col justify-between gap-8 rounded-2xl p-4 shadow-sm lg:flex-row">
             <div className="flex flex-col gap-4">
               <div>
-                <h3 className="text-primary-800 text-xl font-semibold">
+                <h3 className="text-primary-600 text-xl font-semibold">
                   {t("tenant-info")}
                 </h3>
                 <div className="mt-4 flex flex-col items-start gap-1">
@@ -113,7 +115,7 @@ export function UnitModal({ unitId }: UnitModalProps) {
                 </div>
               </div>
               {/* <div>
-                <h3 className="text-primary-800 text-xl font-semibold">
+                <h3 className="text-primary-600 text-xl font-semibold">
                   Unit address
                 </h3>
                 <div className="mt-4 flex flex-col items-start gap-1">
@@ -125,7 +127,7 @@ export function UnitModal({ unitId }: UnitModalProps) {
             </div>
             {contract.from && contract.to && (
               <div>
-                <h3 className="text-primary-800 text-xl font-semibold">
+                <h3 className="text-primary-600 text-xl font-semibold">
                   {t("contract-duration")}
                 </h3>
                 <div className="mt-4 flex items-center gap-4">
@@ -161,7 +163,7 @@ export function UnitModal({ unitId }: UnitModalProps) {
             )}
           </div>
           <div className="rounded-2xl p-4 shadow-sm">
-            <h3 className="text-primary-800 text-xl font-semibold">
+            <h3 className="text-primary-600 text-xl font-semibold">
               {t("rent-collected")}
             </h3>
             <div className="mt-4 flex max-w-80 items-center gap-4">
@@ -182,7 +184,7 @@ export function UnitModal({ unitId }: UnitModalProps) {
         </div>
         <div className="rounded-2xl p-4 shadow-sm">
           <div className="flex items-center justify-between">
-            <h3 className="text-primary-800 text-xl font-semibold">
+            <h3 className="text-primary-600 text-xl font-semibold">
               {t("latest-transactions")}
             </h3>
           </div>
@@ -242,10 +244,25 @@ function CalendarIcon(props: SVGProps<SVGSVGElement>) {
       />
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width=".75"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth=".75"
         d="M4 1.498v1M12 1.498v1"
+      />
+    </svg>
+  );
+}
+
+function HomeIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" {...props}>
+      <path
+        d="M261.56 101.28a8 8 0 00-11.06 0L66.4 277.15a8 8 0 00-2.47 5.79L63.9 448a32 32 0 0032 32H192a16 16 0 0016-16V328a8 8 0 018-8h80a8 8 0 018 8v136a16 16 0 0016 16h96.06a32 32 0 0032-32V282.94a8 8 0 00-2.47-5.79z"
+        fill="currentColor"
+      />
+      <path
+        d="M490.91 244.15l-74.8-71.56V64a16 16 0 00-16-16h-48a16 16 0 00-16 16v32l-57.92-55.38C272.77 35.14 264.71 32 256 32c-8.68 0-16.72 3.14-22.14 8.63l-212.7 203.5c-6.22 6-7 15.87-1.34 22.37A16 16 0 0043 267.56L250.5 69.28a8 8 0 0111.06 0l207.52 198.28a16 16 0 0022.59-.44c6.14-6.36 5.63-16.86-.76-22.97z"
+        fill="currentColor"
       />
     </svg>
   );
