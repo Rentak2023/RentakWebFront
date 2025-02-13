fd9752cd72d3b058c0857681563b9dbc5404bcbf
Amr Gad <merodiro@gmail.com>
2025-02-13 14:24:34 +0200

    update property request

--- src/app/[locale]/tenant/components/property-request.tsx
+++ src/app/[locale]/tenant/components/property-request.tsx
@@ -6,15 +6,17 @@ import { Link } from "@/i18n/routing";
 export default function PropertyRequest() {
   const t = useTranslations("tenant.property-request");
   return (
-    <div className="mx-auto mt-32 flex max-w-7xl flex-col items-center justify-between gap-y-12 px-4 sm:px-6 lg:flex-row lg:px-8">
+    <div className="mx-auto mt-32 flex max-w-7xl flex-col items-center justify-between gap-y-6 px-4 text-center sm:px-6 lg:px-8">
       <div>
-        <p className="text-primary-800 text-lg font-medium">{t("question")}</p>
-        <h2 className="text-primary-900 mt-6 text-4xl font-semibold tracking-tight">
+        <p className="text-primary-900 text-lg font-medium">{t("question")}</p>
+        <h2 className="text-primary-800 mt-4 text-5xl font-semibold tracking-tight">
           {t("title")}
         </h2>
-        <p className="text-primary-800 mt-6">{t("description")}</p>
+        <p className="text-primary-800 mt-6 max-w-lg text-xl">
+          {t("description")}
+        </p>
       </div>
-      <Button variant="dark" size="lg" asChild>
+      <Button size="lg" asChild>
         <Link href="/request-property">{t("cta")}</Link>
       </Button>
     </div>
