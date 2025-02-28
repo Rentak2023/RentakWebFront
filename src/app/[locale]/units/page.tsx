a9bcb2ba6c60fb443930dd8fa2d1168e88de36b1
Amr Gad <merodiro@gmail.com>
2025-03-01 01:34:52 +0200

    improve units listing metadata

--- src/app/[locale]/units/page.tsx
+++ src/app/[locale]/units/page.tsx
@@ -1,7 +1,8 @@
 import { type Metadata } from "next";
 import { type Locale } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
+import { getTranslations, setRequestLocale } from "next-intl/server";
 import { createSerializer, type SearchParams } from "nuqs/server";
+import type { ItemList, WebPage, WithContext } from "schema-dts";
 
 import Properties from "@/components/units/properties/properties";
 import SearchForm from "@/components/units/search-form/search-form";
@@ -22,6 +23,10 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   const { locale } = await props.params;
   const searchParams = await props.searchParams;
+  const t = await getTranslations({
+    locale,
+    namespace: "units.meta",
+  });
 
   const parsedParams = propertiesQueryCache.parse(searchParams);
 
@@ -29,10 +34,18 @@ export async function generateMetadata(
   const totalPages = Math.ceil(properties.total_count / 10);
 
   return {
+    title: t("title"),
+    description: t("description"),
     alternates: generateAlternatesLinks(
       `/units${serialize(searchParams)}`,
       locale,
     ),
+    openGraph: {
+      title: t("title"),
+      description: t("description"),
+      url: `/units${serialize(searchParams)}`,
+      type: "website",
+    },
     pagination: {
       next:
         parsedParams.page < totalPages
@@ -55,14 +68,60 @@ export default async function UnitsPage(
   const searchParams = await props.searchParams;
   const params = await props.params;
 
-  propertiesQueryCache.parse(searchParams);
-
   const { locale } = params;
 
   setRequestLocale(locale);
 
+  const parsedParams = propertiesQueryCache.parse(searchParams);
+  const properties = await getProperties({ ...parsedParams, lang: locale });
+
+  const structuredData: Array<WithContext<WebPage | ItemList>> = [
+    {
+      "@context": "https://schema.org",
+      "@type": "WebPage",
+      name: "Rental Units Search",
+      description: "Search for rental units in your area",
+      url: "https://rentak.com/units",
+      potentialAction: {
+        "@type": "SearchAction",
+        target: {
+          "@type": "EntryPoint",
+          urlTemplate: "https://rentak.com/units?keyword={search_term_string}",
+        },
+        query: "required name=search_term_string",
+      },
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "ItemList",
+      itemListElement: properties.items.map((property, index) => ({
+        "@type": "ListItem",
+        position: index + 1,
+        item: {
+          "@type": "Product",
+          name: property.property_name,
+          description: property.property_description,
+          url: `https://rentak.com/units/${property.id}`,
+          image: property.picture,
+          offers: {
+            "@type": "Offer",
+            price: property.price.toString(),
+            priceCurrency: "EGP",
+            availability: "InStock",
+          },
+        },
+      })),
+      numberOfItems: properties.total_count,
+    },
+  ];
+
   return (
     <main>
+      <script
+        type="application/ld+json"
+        // eslint-disable-next-line @eslint-react/dom/no-dangerously-set-innerhtml
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+      />
       <div className="flex flex-col lg:flex-row">
         <SearchForm />
         <Properties />
